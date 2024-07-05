2182933284f97e7ce27a3dbdc5970b37c14e5ccc
abhishekmano <abhishekmanoharan1999@gmail.com>
2024-07-05 18:59:52 +0530

    Navs Added for mobile navs

--- components/layout/MobileNav.jsx
+++ components/layout/MobileNav.jsx
@@ -3,7 +3,7 @@ import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
-// import { paths } from "./Nav";
+import { paths } from "./Nav";
 const MobileNav = () => {
   const pathname = usePathname();
 
@@ -13,7 +13,29 @@ const MobileNav = () => {
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
-        <div>hello</div>
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Mano <span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {paths.map((route, idx) => {
+            return (
+              <Link
+                key={idx}
+                href={route.path}
+                className={`${
+                  route.path == pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-xl capitalize  hover:text-accent transition-all `}
+              >
+                {route.name}
+              </Link>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
   );
